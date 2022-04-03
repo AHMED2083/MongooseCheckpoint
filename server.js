59cce4b2716db4be1a281067ee01a0f5f6ae5639
AITSISAID AHMED <med.informat@gmail.com>
2022-04-03 09:36:43 +0100

    del

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express=require("express");
 const app=express()
-const connectDB=require('./connectDB.js');
+const connectDB=require('./connectDB');
 const contact=require('./Model.js');
 connectDB()
 
